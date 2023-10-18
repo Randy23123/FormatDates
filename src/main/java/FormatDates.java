5c8a02669e5e78aea945a1b49ec3fc8be20249be
Randy23123 <rlopez@appdev.yearup.org>
2023-10-18 11:57:29 -0400

    exercise 2 done

--- src/main/java/FormatDates.java
+++ src/main/java/FormatDates.java
@@ -1,5 +1,32 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class FormatDates {
     public static void main(String[] args) {
-        
+        LocalDateTime today = LocalDateTime.now();
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String date1 = today.format(fmt);
+        DateTimeFormatter fmt2  = DateTimeFormatter.ofPattern("dd/MMM/yyyy");
+        String date2 = today.format(fmt2);
+        DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd-MMM-yyyy");
+        String date3 = today.format(fmt3);
+        DateTimeFormatter fmt4 = DateTimeFormatter.ofPattern("E,MMM dd yyyy HH:mm");
+        String date4 = today.format(fmt4);
+
+        System.out.println("This is the local time: ");
+
+        System.out.println(date1);
+        System.out.println(date2);
+        System.out.println(date3);
+        System.out.println(date4);
+
+
+
+        //09/05/2021
+        //2021-09-05
+        //September 5, 2021
+        //Sunday, Sep 5, 2021
+        //10:02
     }
 }
